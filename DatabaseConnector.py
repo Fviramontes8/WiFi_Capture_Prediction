36e5a8763a1fc18173ac7fb25d0f1700c2b5c575
Fviramontes8 <kuolemajarvi@yahoo.com>
2018-06-09 13:44:41 -0600

    Added files

---
+++ DatabaseConnector.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+'''
+Authors: Seth Decker, Francisco Viramontes
+
+Description: This code interfaces to a PostgreSQL database. It can build tables, 
+read tables, and write to tables.
+
+
+Example:
+
+    #Read data table
+>>> import DatabaseConnect as dc
+    
+    database = dc.DatabaseConnect()
+    
+    database.connect()
+    
+    print(database.readDataTable())
+    
+    database.disconnect()
+
+
+    #Write data table
+>>> import DatabaseConnect as dc
+    
+    database = dc.DatabaseConnect()
+    
+    data_table_name = "example_table"
+
+    data_list = []
+    data_list.append((1, 2, "dummy", "dummy", "dummy", "dummy", 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24))  
+    data_list.append((2, 3, "dummy", "dummy", "dummy", "dummy", 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25))
+
+    database.writeData(data_table_name, data_list)
+
+    database.disconnect()
+
+'''
+
+from configparser import ConfigParser
+import os
+import psycopg2
+from psycopg2 import sql
+#import pandas as pd
+
+class DatabaseConnect(object):
+    '''Database Object to interact with PostgreSQL database'''
+    def __init__(self):
+        '''Default Constructor for the class'''
+        self.conn = None
+        ## @var  conn
+        #Variable that determines if connection has been established with database
+       
+        self.data_2ghz_query = "(Key, ts, nou, bits, pkt_num, sigs, dr, phyb, phyg, phyn) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)" 
+        ## @var data_2ghz_query
+        #Variable that is used for tables that contain wifi data on the 2ghz spectrum, query: (Key, ts, nou, bits, pkt_num, sigs, dr, phyb, phyg, phyn)
+        
+    def _checkConnection(self):
+        '''Checks connection with PostgreSQL database'''
+        if self.conn is None:
+            print("No connection established. Use obj.connect() to connect to database.")
+            return False
+        else:
+            return True  
+    
+    def _config(self, filename='database.ini', section='postgresql'):
+        '''Reads database.ini file from current directory to get initialization values to connect to database'''
+        # create a parser
+        parser = ConfigParser()
+        # read config file
+        if os.path.exists(filename):
+            parser.read(filename)
+    
+            # get section, default to postgresql
+            db = {}
+            if parser.has_section(section):
+                params = parser.items(section)
+                for param in params:
+                    db[param[0]] = param[1]
+            else:
+                print("expection called")
+                raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    
+            return db
+        else:
+            print("Configuration file does not exist")
+            return None
+
+    def tableExists(self, cursor, table_name):
+        '''Checks if table exists in database'''
+        cursor.execute("select exists(select * from information_schema.tables where table_name=%s)", (table_name,))
+        return cursor.fetchone()[0]
+
+    ## Public Functions    
+    def getNextKey(self, table_name):
+        '''Gets the next key needed to upload to the database, insures that there are no key conflicts'''
+        if self._checkConnection():
+            cur = self.conn.cursor()
+            query = sql.SQL("SELECT MAX(Key) from {}").format(sql.Identifier(table_name))
+            cur.execute(query)
+            latest_key = cur.fetchall()
+            res_list = [x[0] for x in latest_key]
+            return res_list[0]
+    
+    def readTable(self, table_name):
+        '''Returns the entire content of table_name'''
+        if self._checkConnection():
+            cur = self.conn.cursor()
+            query = sql.SQL("select * from {} as a").format(sql.Identifier(table_name))
+            cur.execute(query)
+            print("Data retrieved from table: " + table_name)
+        
+            output = cur.fetchall()
+            
+            return output
+        return
+    
+    def _writeData(self, table_name, query, new_data):
+        #print(query)
+        #print(table_name)
+        #print(new_data)
+        
+        if self._checkConnection():
+            cur = self.conn.cursor()
+            
+            sql_query = sql.SQL("INSERT INTO {} " + query).format(sql.Identifier(table_name))
+                
+            # need to discriminate single and multiple data
+            cur.executemany(sql_query, new_data)
+        
+            self.conn.commit()
+    
+    def writeData(self, table_name, new_table_data):
+        '''Writes data to table of PostgreSQL database, input must be in this from: Key, ts, nou, bits, pkt_num, sigs, dr, phyb, phyg, phyn. Where all values are integers'''
+        self._writeData(table_name, self.data_2ghz_query, new_table_data)
+    #'''
+    def writeDeviceData(self, table_name, table_data):
+        '''This function takes a list of [key, mac addr, ip addr] specified by table_data and adds it to database specified by table_name'''
+        table_query = "(Key , mac, ip) VALUES (%s, %s, %s)"
+        if self._checkConnection():
+            cur = self.conn.cursor()
+            query = sql.SQL("INSERT INTO {} " + table_query).format(sql.Identifier(table_name))
+            cur.execute(query, table_data)
+            self.conn.commit()
+    #'''
+        
+    def _deleteData(self, key, table_name):
+        '''Deletes data specified by key and table_name'''
+        if self._checkConnection():
+            cur = self.conn.cursor()
+            query = sql.SQL("delete from {} where Key={}").format(sql.Identifier(table_name), sql.Identifier(key))
+            cur.execute(query)
+               
+    def connect(self):
+        '''Tries to establish a connection with the database'''
+        try:
+            params = self.config()
+    
+            if params is None:
+                print("Unable to connect to the database! No Params.")
+                return
+    
+            self.conn = psycopg2.connect(**params)
+            print("Connected.")
+        except:
+            print("Unable to connect to the database!")
+
+    def disconnect(self):
+        '''Disconnects from the database'''
+        if self.conn is not None:
+            self.conn.close()
+        print("Disconnected.")
+    
+
+    def createDataTable(self, table_name):
+        '''Creates table specified by table_name with columns: (Key, ts, nou, bits, pkt_num, sigs, dr, phyb, phyg, phyn). They are all integer values'''
+        ##@var table_name
+        #Name of the table the user wants to get data from in database
+        if self._checkConnection():
+            #moving key->value to post processing
+            args = "(Key INT PRIMARY KEY, ts INT, nou INT, bits INT, pkt_num INT, sigS INT, dr INT, phyb INT, phyg INT, phyn INT)"
+            query = sql.SQL("CREATE TABLE {} " + args).format(sql.Identifier(table_name))            
+            self.conn.cursor().execute(query)
+            self.conn.commit()
+
+    def getTableNames(self):
+        '''Returns a list of table names in the database'''
+        #In order to see tables names the user should print it
+        #print DatabaseConnect.getTableNames()
+        if self._checkConnection():
+            cur = self.conn.cursor()
+    
+            cur.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
+            out = cur.fetchall()
+            table_names = [x[0] for x in out]
+            return table_names
---
+++ Gaussian_Process_Predictor.py
@@ -0,0 +1,318 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Fri Sep 01 11:06:34 2017
+
+@author Francisco Viramontes
+'''
+import DatabaseConnector as dc
+import numpy as np
+np.set_printoptions(threshold=np.nan)
+#import scipy as sp
+#import pylab as pb
+import matplotlib.pyplot as plt
+#from random import seed
+from sklearn.gaussian_process import GaussianProcessRegressor
+#from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import DotProduct as LK, WhiteKernel as WK
+from sklearn.gaussian_process.kernels import ConstantKernel as CK, Sum
+#from sklearn.gaussian_process.kernels import RationalQuadratic as RQ, ExpSineSquared as ESS 
+
+def mean(values):
+    '''Determines the mean of an array'''
+    if len(values) > 0:
+        return sum(values) / float(len(values))
+    else:
+        print("Usage:\n\tmean(values)\n\t\tvalues is an array of length greater than 0")
+        return 0
+
+def sample_var(values, sample_mean=None):
+    '''Takes the sum of means and divides by the length of the array'''
+    if sample_mean == None:
+        sample_mean = mean(values)
+    return sum([(x-sample_mean)**2 for x in values])/len(values)
+
+def covariance(x, mean_x, y, mean_y):
+    '''Determines the covariance of two functions, returns 0 otherwise'''
+    cov = 0.0
+    for i in range(len(x)):
+        cov += (x[i] - mean_x) * (y[i] - mean_y)
+    return cov
+
+def sub_sample(sample_arr, sample_size):
+    '''Gets every nth elements, it also includes the last element of the array'''
+    new_sample = sample_arr
+    return_sample = []
+    q = (sample_size - 1)
+    while q < len(sample_arr):
+        return_sample.append(new_sample[q])
+        q += sample_size
+        
+        if q >= len(sample_arr) - 1:
+            '''
+            q = len(sample_arr) - 1
+            return_sample.append(new_sample[q])
+            '''
+            return_sample.append(new_sample[-1])
+            break
+    return return_sample
+
+def avg_sample(sample_arr, sample_size):
+    '''Gets a chunk (like 5 values for example) takes the average of the chunk, then it is added to an array as one value. Continues to do this for the rest of the array.
+    Example: array = [2, 2, 16, 4, 5, 15]
+    result_value = avg_sample(array, 4)
+    result_value is [6, 8]'''
+    a = sample_arr
+    sample_return = []
+    j = 0
+    k = sample_size - 1
+    while k < len(a):
+        m = []
+        for i in range(j, k+1):
+            m.append(a[i])
+        sample_return.append(int(mean(m)))
+        j += sample_size - 1
+        k += sample_size - 1
+        if k >= len(a):
+            m = []
+            k = len(a)
+            for i in range(j+1, k):
+                m.append(a[i])
+            sample_return.append(int(mean(m)))
+            break
+    return np.atleast_1d(sample_return)
+
+def grab_n(array, n):
+    '''Gets the first n values of an array, returns error if n is greater than the length of the array'''
+    if n < len(array):
+        return np.atleast_1d([array[i] for i in range(n)]).T
+    else:
+        print("Usage: \n\tgrab_n(array, n), n must be less than the length of array")
+        return 0
+
+def grab_nz(array, n ,z):
+    '''Gets the first n values of an array, returns error if n is greater than the length of the array or if z < n or z > len(array)'''
+    if n <= len(array):
+        if z <= len(array):
+            return np.atleast_1d([array[i] for i in range(n, z)]).T
+        else:
+            print("Usage: \n\tgrab_nz(array, n, z)\n\t\tn must be less than the length of array and n < z < len(array)")
+            return 0
+    else:
+        print("Usage: \n\tgrab_nz(array, n, z)\n\t\tn must be less than the length of array")
+        return 0
+
+#(nou, nou_tst, 60, 0, 100, 15)
+def GP_prep(train, test, avg_samp, sub_samp_begin, sub_samp_end, window):
+    '''Inputs: train/test, which needs to be an array and each can be a different size, avg_samp is 
+     an integer that is going to sub-sample train/test (ex. if avg_samp = 60, it will sample every 60 values as one) so 
+     avg_samp < length of train/test, sub_samp_begin/sub_samp_end specifies the total bounds of what the user wants 
+     to keep from the sub-sampled result from avg_samp, so sub_samp_begin/sub_samp_end < length of train/test, the window
+     specifies how wide the resultant matrix of this function is.
+     Output: Length of total sampled values, training and test matricies that has window of averaged sampled values,
+         and a graph-able array of what the training values look like (ycomp)
+     Example: window = 5, length of array input (both train and test are same size in this example) = n
+         [x_0 x_1 ... x_4]          [x_5]
+         [x_1 x_2 ... x_5]          [x_6]
+     x = [x_2 x_3 ... x_6]     y =  [x_7]
+         [... ... ... ...]          [...]
+         [x_n-5-1... ... x_n-1]     [x_n]
+         '''
+    samp_train = avg_sample(train, avg_samp)
+    tot_samp = len(samp_train)
+    tr = np.atleast_2d([grab_nz(samp_train, m, n) for m, n in zip(range(samp_train.shape[0]), range(window, samp_train.shape[0]))])
+    Xtr = np.atleast_2d(tr)
+    ob = np.atleast_2d([[samp_train[i] for i in range(window, samp_train.shape[0])]])
+    Ytr = np.atleast_2d(ob).T 
+    samp_test = avg_sample(test, avg_samp) 
+    samp_test1 = grab_nz(samp_test, sub_samp_begin, sub_samp_end) 
+    feat_xtest = [grab_nz(samp_test1, m, n) for m, n in zip(range(samp_test1.shape[0]), range(window, samp_test1.shape[0]))]
+    xtst =  np.atleast_2d(feat_xtest)
+    feat_comp = [samp_train[i] for i in range(window, samp_test1.shape[0])]
+    ycomp = np.atleast_2d(feat_comp).T
+    feat_ytest = [samp_test[i] for i in range(window, samp_test1.shape[0])]
+    ytst = np.atleast_2d(feat_ytest).T
+    return tot_samp, Xtr, Ytr, xtst, ycomp, ytst
+    
+#Main:
+timestamps = []
+nou = []
+nou_tst = []
+bits = []
+bits_tst = []
+pktNum = []
+pkt_tst = []
+sigS = []
+sigS_tst = []
+dataRate = []
+dR_tst = []
+phyB = []
+b_tst = []
+phyG = []
+g_tst = []
+phyN = []
+n_tst = []
+
+db = dc.DatabaseConnect()
+db.connect()
+
+train_table = "pi_mon"
+test_table = "pi_sun"
+
+train = db.readTable(train_table)
+test = db.readTable(test_table)
+
+#create_table_name = "pi_mon"
+#db.createDataTable(create_table_name)
+
+db.disconnect()
+
+#Data from table (in form of tuple)
+#k and l are just dummy arrays
+for k in sorted(train, key=lambda hello: hello[0]):
+    timestamps.append(int(k[1]))
+    nou.append(int(k[2]))
+    bits.append(int(k[3]))
+    pktNum.append(int(k[4]))
+    sigS.append(int(k[5]))
+    dataRate.append(int(k[6]))
+    phyB.append(int(k[7]))
+    phyG.append(int(k[8]))
+    phyN.append(int(k[9]))
+    
+for l in sorted(test, key=lambda yello: yello[0]):
+    nou_tst.append(int(l[2]))
+    bits_tst.append(int(l[3]))
+    pkt_tst.append(int(l[4]))
+    sigS_tst.append(int(l[5]))
+    dR_tst.append(int(l[6]))
+    b_tst.append(int(l[7]))
+    g_tst.append(int(l[8]))
+    n_tst.append(int(l[9]))
+
+
+#print "Average number of users: " + str(int(mean(nou)))
+#print "Standard deviation: " + str(int(np.sqrt(sample_var(nou, mean(nou)))))
+
+training_data=[nou, 
+               bits, 
+               pktNum, 
+               sigS, 
+               dataRate, 
+               phyB, 
+               phyG, 
+               phyN
+               ]
+
+test_data = [nou_tst, 
+             bits_tst, 
+             pkt_tst, 
+             sigS_tst, 
+             dR_tst, 
+             b_tst, 
+             g_tst, 
+             n_tst
+             ]
+
+labels = ["Number of users", 
+          "Bits", 
+          "Number of Packets", 
+          "Signal Strength",
+          "Data Rate(MB)", 
+          "802.11b bits", 
+          "802.11g bits", 
+          "802.11n bits"
+          ]
+
+for iter_1, iter_label in zip(training_data, labels):
+    plt.plot(timestamps, iter_1, "r-")
+    plt.ylabel("Feature: "+iter_label)#("Number of users")
+    plt.xlabel("Timestamp")
+    plt.show()
+
+
+short_nou = avg_sample(nou, 1800)
+short_bits = avg_sample(bits, 1800)
+short_pktNum = avg_sample(pktNum, 1800)
+short_sigS = avg_sample(sigS, 1800)
+short_dR = avg_sample(dataRate, 1800)
+
+'''Uncomment to plot the sampled data
+plt.title("Collected features with half-hour sample rate")
+plt.xlabel("Time")
+plt.ylabel("Signal Strength")
+plt.plot(short_nou, "r-", label="Number of users")
+plt.plot(short_bits, "b--", label="Bits")
+plt.plot(short_pktNum, "g-", label="Number of packets")
+plt.plot(short_sigS, "y--", label="Signal strength")
+plt.plot(short_dR, "c--", label="Data rate")
+plt.legend()
+plt.show()
+'''
+'''
+#Number of test samples
+sample_start = 0
+sample_end = 100
+#Window size
+sample_window = 15
+
+
+kernel1 = LK(sigma_0 = 1, sigma_0_bounds = (1e-1, 1e1))
+kernel2 = CK(constant_value=1)
+kernel3 = WK(0.1)
+kernel = Sum(kernel1, kernel2)
+kernel = Sum(kernel, kernel3)
+#1e-1 for linear + constant, 1e-3 for RBF
+gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10,\
+                              normalize_y=False, alpha=1e-1)
+#print gp.get_params()['kernel']
+
+for z in range(len(labels)): #len(labels)
+    total_samp, Xtr, Ytr, Xtst, Ycomp, Ytst = GP_prep(training_data[z], test_data[z], 60, sample_start, sample_end, sample_window)
+    
+    #print(Xtr.shape, Ytr.shape, Xtst.shape, Ytst.shape)
+    #Testing if it overfits
+    Xtr_1 = [Xtr[i] for i in range(sample_window, sample_end)]
+    
+    #GP_ysample = gp.sample_y(Xtr_1, 1)
+    try:
+        gp.fit(Xtr, Ytr)
+        print "marginal likelihood:", gp.log_marginal_likelihood()
+        y_pred, y_sigma = gp.predict(Xtst, return_std=True)
+        #print(y_pred.shape)
+        
+        result_time = [g+1 for g in range(sample_window, sample_end)]
+        
+        s = "training interval between "+str(result_time[0])+" and "+str(result_time[-1])+\
+        " minutes\n window is "+str(sample_window)
+        plt.xlabel(s=s)
+        ylab = labels[z]
+        plt.ylabel(ylab)
+        plt.title(s="Training data with feature "+str(ylab))
+        plt.plot(result_time, Ycomp, "g-", label="training")
+        plt.legend()
+        plt.show()
+        
+        GP_xlabel = "time interval between "+str(result_time[0])+" and "+str(result_time[-1])+\
+        " minutes\n window is "+str(sample_window)
+        plt.xlabel(s=GP_xlabel)
+        GP_ylabel = labels[z]
+        plt.ylabel(GP_ylabel)
+        GP_dataplot_title = "Using "+str(gp.get_params()['kernel'])+" kernel\nwith "+str(total_samp)+" averaged training samples\nand "+str(sample_end)+\
+        " averaged test samples"
+        plt.title(s=GP_dataplot_title)
+        
+        #ploting data
+        #plt.plot(result_time, GP_ysample, "c-", label= "kernel sample")
+        plt.plot(result_time, y_pred.T[0], "c-", label="predicted")
+        plt.plot(result_time, Ytst.T[0], "y-", label="real")
+        plt.fill(np.concatenate([result_time, result_time[::-1]]),
+                 np.concatenate([y_pred-1.96*y_sigma,
+                                (y_pred+1.96*y_sigma)[::-1]]),
+                 alpha=.5, fc='b', ec='none')
+        plt.legend()
+        plt.show()
+        
+    except:
+        print "Feature "+str(labels[z])+" did not work!"
+        continue
+'''
