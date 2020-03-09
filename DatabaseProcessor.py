f0faae3af69d3160782b71e870bd8fc1e1d45ced
Francisco Viramontes <kuolemajarvi@yahoo.com>
2020-03-09 11:42:03 -0600

    Took some functions in GP_Predictor_5GHz.py and moved them to DatabaseProcessor.py and SignalProcessor.py

---
+++ DatabaseProcessor.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 11:50:08 2020
+
+Packages needed: scikit-learn, psycopg2, numpy, scipy
+
+@author: frankie
+From: https://github.com/fviramontes8/Wifi_Capture_Prediction
+Depends on file: Signal Processor.py
+"""
+
+#Package to interface with AWS database
+import DatabaseConnector as dc
+
+#Private signal processor/sampler
+import SignalProcessor as sp
+
+import numpy as np
+
+def print_table_names():
+	database = dc.DatabaseConnect()
+	database.connect()
+	table_names = database.getTableNames()
+	print(table_names)
+	database.disconnect()
+
+def create_table(table_name):
+	database = dc.DatabaseConnect()
+	database.connect()
+	database.createDataTable_5ghz(table_name)
+	database.disconnect()
+
+def del_table(table_name):
+	db = dc.DatabaseConnect()
+	db.connect()
+	db.drop_table(table_name)
+	db.disconnect()
+
+
+def read_5ghz_day(table_name):
+	'''
+	Input: A string that describes a table name.
+	Output: A touple of lists.
+	Description: Gets the contents of the table if it exists. For this function
+		specifically, there is an assumption that the table has a format of
+		such: it is a nx9 table with column names (Key, ts, nou, bits, pkt_num,
+		sigs, dr, phya, phyn) and is in the file 'databse.ini'. Please look at
+		documentation for DatabaseConnect() and _config() for more information.
+	'''
+	db = dc.DatabaseConnect()
+	db.connect()
+	data = db.readTable(table_name)
+	#print("Next key is:", db.getNextKey(table_name))
+	db.disconnect()
+
+	t_stamps = []
+	num_of_users = []
+	bits = []
+
+	for db_iter in sorted(data, key=lambda dummy_arr:dummy_arr[1]):
+		t_stamps.append(db_iter[1])
+		num_of_users.append(db_iter[2])
+		bits.append(db_iter[3])
+
+	return_data = [t_stamps,
+					num_of_users,
+					bits
+					]
+	return return_data
+
+def week_data_prep(day, start_week, end_week, sample_rate, sample_rate2=None):
+	training_data = []
+	labels_5ghz = ["Number of users",
+       				"Bits"
+       				]
+
+	for week in range(start_week, end_week+1):
+		table_name = "5pi_"+str(day)+str(week)
+		day_data = read_5ghz_day(table_name)
+
+		while(day_data[2][0] < 1):
+				del day_data[2][0]
+		filtered_data = sp.butterfilter(day_data[2], labels_5ghz[1], table_name)
+		sampled_data = sp.sub_sample(filtered_data, labels_5ghz[1], table_name, sample_rate)
+		if(sample_rate2):
+			sampled_data = sp.sub_sample(sampled_data, labels_5ghz[1], table_name, sample_rate2)
+		training_data.extend(sampled_data)
+
+	training_data = np.array(training_data)
+	return training_data
+
+def day_data_prep(days_of_week, num_of_weeks, sample_rate, sample_rate2=None):
+	training_data = []
+	labels_5ghz = ["Number of users",
+       				"Bits"
+       				]
+	for week_num in range(2, num_of_weeks+1):
+		for day in days_of_week:
+			table_name = "5pi_"+str(day)+str(week_num)
+			day_data = read_5ghz_day(table_name)
+
+			while(day_data[2][0] < 1):
+				del day_data[2][0]
+			filtered_data = sp.butterfilter(day_data[2], labels_5ghz[1], table_name)
+			sampled_data = sp.sub_sample(filtered_data, labels_5ghz[1], table_name, sample_rate)
+			if(sample_rate2):
+				sampled_data = sp.sub_sample(sampled_data, labels_5ghz[1], table_name, sample_rate2)
+			training_data.extend(sampled_data)
+
+	training_data = np.array(training_data)
+	return training_data
---
+++ SignalProcessor.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 15:12:31 2020
+Packages needed: matplotlib, numpy, scipy
+
+@author: frankie
+From: https://github.com/fviramontes8/Wifi_Capture_Prediction
+"""
+
+#For matrix and linear algebra calcualtions
+import numpy as np
+
+#For use of the butterworth and Savgol filters
+from scipy import signal
+
+#For ploting data
+import matplotlib.pyplot as plt
+
+def mean(values):
+    '''Determines the mean of an array'''
+    if len(values) > 0:
+        return sum(values) / float(len(values))
+    else:
+        print("Usage:\n\tmean(values)\n\t\tvalues is an array of length greater than 0")
+        return 0
+
+def grab_nz(array, n ,z):
+	'''Gets the first n to z values of an array, returns error if n is greater
+		than the length of the array or if z < n or z > len(array)
+	'''
+	if (n <= len(array)) and (z <= len(array)):
+		return np.atleast_1d([array[i] for i in range(n, z)]).T
+	else:
+		print("Usage: \n\tgrab_nz(array, n, z)\n\t\tn must be less than the length of array and n < z < len(array)")
+		return -1
+
+def butterfilter(input_arr, title, day, freq=60):
+	'''
+	Input:
+		A list that can be represented as a time series that is the feature
+			desired to be filtered (input_arr)
+	Output:
+		A new list that are a filtered version of the input. It is the
+			same length as the input
+	'''
+	z = (0.9/4) / freq
+	begin_cutoff = 0
+	b, a = signal.butter(6, z, 'low')
+	xf = signal.lfilter(b, a, input_arr)
+	graphing = 0
+	if graphing:
+		plt.plot(input_arr[begin_cutoff:], label="Original Data")
+		plt.plot(xf[begin_cutoff:], label="Filtered Data")
+		plt.title("Filtered "+title+" for "+day)
+		plt.ylabel(title)
+		plt.xlabel("Time of day (seconds)")
+		plt.legend()
+		plt.show()
+	return xf
+
+def sub_sample(xf, title, day, sampling=60):
+	'''
+	Input:
+		A string that describes the filter (title)
+		A sampling frequency (sampling) [the default value is 60 to sample
+			the data into minute chunks]
+	Output: A list of filtered data points (1/sampling) of original size
+	'''
+	xf_copy = np.array(xf).copy()
+	xs = xf_copy[1::sampling]
+	x_axis_xs = np.array([i for i in range(1, len(xf), sampling)])
+	graphing = 0
+	if graphing:
+		plt.title("Subsampled data at a rate of " +str(sampling)+" for "+day)
+		plt.ylabel(title)
+		plt.xlabel("Time of day (seconds)")
+		plt.plot(x_axis_xs, xs, "g", label="Sampled data")
+		plt.legend()
+		plt.show()
+	return xs
+
+def avg_sample(sample_arr, sample_size):
+    '''Gets a chunk (like 5 values for example) takes the average of the chunk, then it is added to an array as one value. Continues to do this for the rest of the array.
+    Example: array = [2, 2, 16, 4, 5, 15]
+    result_value = avg_sample(array, 4)
+    result_value is [6, 8]'''
+    a = sample_arr
+    sample_return = []
+    j = 0
+    k = sample_size - 1
+    while k < len(a):
+        m = []
+        for i in range(j, k+1):
+            m.append(a[i])
+        sample_return.append(int(mean(m)))
+        j += sample_size - 1
+        k += sample_size - 1
+        if k >= len(a):
+            m = []
+            k = len(a)
+            for i in range(j+1, k):
+                m.append(a[i])
+            sample_return.append(int(mean(m)))
+            break
+    return np.atleast_1d(sample_return)
+
+def savgol(input_arr, title):
+	'''
+	DEPRECATED
+	'''
+	sampling = 60
+	xf = signal.savgol_filter(input_arr, 5, 2)
+	xf_copy = np.array(xf).copy()
+	xs = xf_copy[1::sampling]
+	x_axis_xs = np.array([i for i in range(len(xf))])
+	x_axis_xs = x_axis_xs[::sampling]
+	plt.plot(input_arr, label="Original Data")
+	plt.plot(x_axis_xs, xs, label="Filtered Data")
+	plt.title("Time series and filtered data for "+title)
+	plt.ylabel(title)
+	plt.xlabel("Time of day (seconds)")
+	plt.legend()
+	plt.show()
+	return xs
