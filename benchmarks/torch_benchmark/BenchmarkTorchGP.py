677dc917d1245dceaaf58d4e401e55be2a5245cb
fviramontes8 <kuolemajarvi@yahoo.com>
2020-06-20 12:10:31 -0600

    Created a benchmark folder for both pytorch and scikit-learn GP, the torch is almost completed

--- benchmarks/torch_benchmark/BenchmarkTorchGP.py
+++ benchmarks/torch_benchmark/BenchmarkTorchGP.py
@@ -12,7 +12,9 @@ class ExactGPModel(gpytorch.models.ExactGP):
 	def __init__(self, train_x, train_y, likelihood):
 		super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
 		self.mean_module = gpytorch.means.ConstantMean()
-		self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.LinearKernel())
+		self.covar_module = gpytorch.kernels.ScaleKernel(
+			gpytorch.kernels.LinearKernel()
+		)
 		
 	def forward(self, x):
 		mean_x = self.mean_module(x)
@@ -57,11 +59,38 @@ def PlotGPPred(XCompare, YCompare, XPred, YPred, Title=""):
 		lower_sigma, upper_sigma = YPred.confidence_region()
 		ax.plot(XCompare.numpy(), YCompare.numpy(), "k.")
 		ax.plot(XPred.numpy(), YPred.mean.numpy(), "b")
-		ax.fill_between(xtst.numpy(), lower_sigma.numpy(), upper_sigma.numpy(), alpha = 0.5)
+		ax.fill_between(
+			xtst.numpy(), 
+			lower_sigma.numpy(), 
+			upper_sigma.numpy(), 
+			alpha = 0.5
+		)
 		ax.set_ylim([-10, 10])
 		ax.set_xlim([-6, 6])
 		ax.set_title(Title)
 		ax.legend(["Observed Data", "Mean", "Confidence"])
+		
+def verify_confidene_region(YPred, YTrue):
+	y_pred_mean = YPred.mean.numpy()
+	y_true = YTrue.numpy()
+	assert (len(y_pred_mean) == len(YTrue))
+	
+	y_lower_sigma, y_upper_sigma = YPred.confidence_region()
+	y_lower_sigma1, y_upper_sigma1 = y_lower_sigma.numpy() / 1.96, y_upper_sigma.numpy() / 1.96
+	y_lower_sigma2, y_upper_sigma2 = y_lower_sigma.numpy(), y_upper_sigma.numpy()
+	
+	sigma1_count = 0
+	sigma2_count = 0
+	
+	for i in range(len(y_pred_mean)):
+		if y_lower_sigma1[i] <= y_true[i] and y_upper_sigma1[i] >= y_true[i]:
+			sigma1_count += 1
+		
+		if y_lower_sigma2[i] <= y_true[i] and y_upper_sigma2[i] >= y_true[i]:
+			sigma2_count += 1
+			
+	return sigma1_count/len(YTrue), sigma2_count/len(YTrue)
+	
 
 if __name__ == "__main__":	
 	xtr = torch.linspace(-2, 2, 10)
@@ -83,11 +112,22 @@ if __name__ == "__main__":
 		lr = 0.1
 	)
 	
-	model, likelihood = TorchTrain(xtr, ytr, model, likelihood, optimizer, training_iter)	
+	model, likelihood = TorchTrain(
+		xtr, 
+		ytr, 
+		model, 
+		likelihood, 
+		optimizer, 
+		training_iter
+	)	
 	pred = TorchTest(xtst, model, likelihood)
+	print(type(pred))
 		
-	PlotGPPred(xtr, ytr, xtst, pred)
-	PlotGPPred(xtst, ytst, xtst, pred)
+	gp_title = "Linear GP Prediction\nwith 2 standard deviations"
+	PlotGPPred(xtr, ytr, xtst, pred, gp_title)
+	PlotGPPred(xtst, ytst, xtst, pred, gp_title)
+	
+	print("1 standard deviation/2 standard deviation confidence:", verify_confidene_region(pred, ytst))
 	
 	xtr2 = torch.linspace(-6, 6, 10)
 	ytr2 = xtr2 + torch.randn(len(xtr2))
@@ -96,20 +136,18 @@ if __name__ == "__main__":
 	
 	model = ExactGPModel(xtr2, ytr2, likelihood)
 	
-	model, likelihood = TorchTrain(xtr2, ytr2, model, likelihood, optimizer, training_iter)	
+	model, likelihood = TorchTrain(
+		xtr2, 
+		ytr2, 
+		model, 
+		likelihood, 
+		optimizer, 
+		training_iter
+	)	
 	pred = TorchTest(xtst2, model, likelihood)
 
-	"""
-	l, u = pred.confidence_region()
-	print(len(pred.mean.numpy()), len(l.numpy()), len(u.numpy()))
-	z = []
-	np_pred = pred.mean.numpy()
-	for i in range(len(np_pred)):
-		if np_pred[i] > -2 and np_pred[i] < 2:
-			z.append(np_pred[i])
-		
-	print(len(z))
-	"""
 	
-	PlotGPPred(xtr2, ytr2, xtst2, pred)
-	PlotGPPred(xtst2, ytst2, xtst2, pred)
+	PlotGPPred(xtr2, ytr2, xtst2, pred, gp_title)
+	PlotGPPred(xtst2, ytst2, xtst2, pred, gp_title)
+	
+	print("1 standard deviation/2 standard deviation confidence:", verify_confidene_region(pred, ytst2))
