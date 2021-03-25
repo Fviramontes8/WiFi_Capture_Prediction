ee28ec6862272fd8c4e9374ce9f2dbb051a56688
fviramontes8 <kuolemajarvi@yahoo.com>
2021-03-24 19:02:00 -0600

    Data is now read in sampled and filtered, rather than sampled, filtered, and normalized. Normalization is done in the program to remember mean and standard deviation. A plot of the unnormalized data is at the end of the program.

--- GP_Predictor_5GHz_Pytorch.py
+++ GP_Predictor_5GHz_Pytorch.py
@@ -7,9 +7,7 @@ From: https://github.com/fviramontes8/Wifi_Capture_Prediction
 Depends on files: DatabaseConnector.py, DatabaseProcessor.py Signal Processor.py
 """
 # TODO: 
-#	Try batch GP
-#	Run code with MAPE function, set up a loop for different parameters
-#    (and differnt cv folds) to find best ridge model.
+#	Try r-squared, mean absolute error, and student T
 
 # Private signal processor/sampler
 import SignalProcessor as sp
@@ -24,6 +22,10 @@ import numpy as np
 
 # Mean squared error to determine quality of prediction
 from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import mean_absolute_error as mae
+from sklearn.metrics import r2_score
+
+from scipy.stats import ttest_ind
 
 import torch
 import gpytorch
@@ -43,20 +45,22 @@ class ExactGPModel(gpytorch.models.ExactGP):
 
 if __name__ == "__main__":
 	test_day = "Monday"
-	bits_train = np.load("data/tr_bits_15weeks_hoursample_normalized.npy")
-	bits_test = np.load("data/tst_bits_week15mon_hoursample_normalized.npy")
+	bits_train_raw = np.load("data/tr_bits_15weeks_hoursample.npy")
+	bits_train, tr_mu, tr_sig = sp.std_normalization(bits_train_raw, 1)
+	bits_test_raw = np.load("data/tst_bits_week15mon_hoursample.npy")
+	bits_test, tst_mu, tst_sig = sp.std_normalization(bits_test_raw, 1)
 	print(len(bits_train))
 	print(len(bits_test))
 	#Parameters for formatting training data
 	window = 3
 
 	bits_train = sp.buffer(bits_train, window+1, window)
-	print(bits_train.shape)
+	#print(bits_train.shape)
 	Xtr = bits_train[:window, :]
 	#Xtr = bits_train[0, :]
 	Ytr = bits_train[window, :]
 	bits_test = sp.buffer(bits_test, window+1, window)
-	print(bits_test.shape)
+	#print(bits_test.shape)
 	Xtst = bits_test[:window, :]
 	Ytst = bits_test[window, :]
 	
@@ -79,28 +83,30 @@ if __name__ == "__main__":
 	
 	Xtst_torch = torch.from_numpy(Xtst).float()
 	Xtst_torch = Xtst_torch.transpose(0, 1)
-	print("Xtst shape:", Xtst_torch.shape, "Ytst shape:", Ytst.shape)
+	#print("Xtst shape:", Xtst_torch.shape, "Ytst shape:", Ytst.shape)
 	
 	torch_pred = gptu.TorchTest(Xtst_torch, model, likelihood)
-	print("Prediction shape:", torch_pred.mean.shape)
+	#print("Prediction shape:", torch_pred.mean.shape)
 	
 	# Setting up data to print the predition results
 	time_test = torch.Tensor([i for i in range(Ytst.shape[0])])
 	
 	gp_title = "Linear GP Prediction\nwith 1 standard deviation\nand 2 standard deviations"
-	x_title = "Time (hours)"
+	x_title = "Time (half-hours)"
 	y_title = "Bits"
 	pu.PlotGPPred(time_test, torch.Tensor(Ytst), time_test, torch_pred, x_title, y_title, gp_title)
-	
-	torch_gp_mape = sp.mape_test(Ytst, torch_pred.mean.numpy())
-	print("Torch mape:", torch_gp_mape)
-	torch_gp_mse = mse(Ytst, torch_pred.mean.numpy())
-	print("Torch GP MSE:", torch_gp_mse)
-	
 	
 	lower_sigma2, upper_sigma2 = torch_pred.confidence_region()
 	lower_sigma1, upper_sigma1 = gptu.ToStdDev1(torch_pred)
 	
 	one_sigma, two_sigma = gptu.verify_confidence_region(torch_pred, torch.Tensor(Ytst))
 	print(one_sigma,"is contained within 1 standard deviation")
-	print(two_sigma, "is contained within 2 standard deviations")
+	print(two_sigma, "is contained within 2 standard deviations\n")
+	
+	# Apply denormalization for all test metrics
+	y_tst_denorm = sp.denormalize(Ytst, tst_mu, tst_sig)
+	t_pred_denorm = sp.denormalize(torch_pred.mean.numpy(), tst_mu, tst_sig)
+	title = "Comparison of denormalized data"
+	xtitle = "Time (hours)"
+	ytitle = "Denormalized bits"
+	pu.general_double_plot(y_tst_denorm, t_pred_denorm, title, xtitle, ytitle)
