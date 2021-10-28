60eb62056e0b85c4a5ba3e0169ef084d4fddd1e1
frankie <fviramontes8@unm.edu>
2021-10-27 19:44:07 -0600

    Add multitask prediction
    The multitask Gaussian Process can now train and predict on a training data that contains multiple features

--- MultiFeatureGP_GPyTorch.py
+++ MultiFeatureGP_GPyTorch.py
@@ -8,8 +8,7 @@ Depends on local files: DatabaseConnector.py, DatabaseProcessor.py Signal Proces
 # Private signal processor/sampler
 import SignalProcessor as sp
 # Private GPyTorch and PyTorch functions
-import GPyTorchUtilities as gptu
-# Private plot functions
+import GPyTorchUtilities as gptu # Private plot functions
 import PlotUtils as pu
 # Private database processor
 import DatabaseProcessor as dbp
@@ -19,13 +18,12 @@ import numpy as np
 #np.set_printoptions(threshold=np.nan)
 
 import csv
-
 # Mean squared error to determine quality of prediction
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import mean_absolute_error as mae
 from sklearn.metrics import r2_score
 
-from scipy.stats import ttest_ind
+#from scipy.stats import ttest_ind
 
 import torch
 import gpytorch
@@ -57,98 +55,132 @@ def window_prep(data, window=10):
 	
 	return X, y
 
-raw_nou_data = np.load("data/raw_nou_15weeks.npy")
-raw_bits_data = np.load("data/raw_bits_15weeks.npy")
-raw_pkt_data = np.load("data/raw_numpkt_15weeks.npy")
-raw_sig_data = np.load("data/raw_sigstrength_15weeks.npy")
-raw_phya_data = np.load("data/raw_phya_15weeks.npy")
-print(len(raw_nou_data))
+if __name__ == "__main__":
+	raw_nou_data = np.load("data/raw_nou_15weeks.npy")
+	raw_bits_data = np.load("data/raw_bits_15weeks.npy")
+	raw_pkt_data = np.load("data/raw_numpkt_15weeks.npy")
+	raw_sig_data = np.load("data/raw_sigstrength_15weeks.npy")
+	raw_phya_data = np.load("data/raw_phya_15weeks.npy")
+	print(len(raw_nou_data))
 
-raw_data = [
-	raw_nou_data,
-	raw_bits_data, 
-	raw_pkt_data, 
-	raw_sig_data,
-	raw_phya_data
-]
+	#pu.general_plot(raw_bits_data, "Raw bits")
+	#pu.general_plot(raw_phya_data, "Raw phya")
 
-raw_titles = [
-	"Number of Users",
-	"Bits",
-	"Number of Packets",
-	"Signal Strength",
-	"802.11a data"
-]
-
-#pu.plot_features(raw_data, raw_titles)
-
-nou = buffer_filter(raw_nou_data)
-bits = buffer_filter(raw_bits_data)
-pktnum = buffer_filter(raw_pkt_data)
-sig = buffer_filter(raw_sig_data)
-phya = buffer_filter(raw_phya_data)
-
-filtered_data = [nou, bits, pktnum, sig, phya]
-filter_str = "Filtered "
-filtered_titles = [
-	filter_str+raw_titles[0],
-	filter_str+raw_titles[1],
-	filter_str+raw_titles[2],
-	filter_str+raw_titles[3],
-	filter_str+raw_titles[4]
-]
+	raw_data = [
+		raw_nou_data,
+		raw_bits_data, 
+		raw_pkt_data, 
+		raw_sig_data,
+		raw_phya_data
+	]
 	
-#pu.plot_features(filtered_data, filtered_titles)
-
-
-#pu.plot_autocorr(nou, "Auto correlation of number of users")
-#pu.plot_autocorr(bits, "Auto correlation of bits")
-#pu.plot_autocorr(pktnum, "Auto correlation of number of packets")
-#pu.plot_autocorr(sig, "Auto correlation of signal strength")
-#pu.plot_autocorr(phya, "Auto correlation of 802.11a bits")
-
-#pu.plot_crosscorr(bits, nou, "Cross correlation between number of users and bits")
-#pu.plot_crosscorr(bits, pktnum, "Cross correlation between bits and number of packets")
-#pu.plot_crosscorr(pktnum, sig, "Cross correlation between number of packets and signal strength")
-
-nou_tr_x, nou_tr_y = window_prep(nou)
-bits_tr_x, bits_tr_y = window_prep(bits)#, 3600)
-pktnum_tr_x, pktnum_tr_y = window_prep(pktnum)
-sig_tr_x, sig_tr_y = window_prep(sig)
-
-phya_tst_x, phya_tst_y = window_prep(phya)
-
-print(nou_tr_x.shape, nou_tr_y.shape)
-
-train_x = torch.stack([
-		nou_tr_x.transpose(0, 1),
-		bits_tr_x.transpose(0, 1),
-		pktnum_tr_x.transpose(0, 1),
-		sig_tr_x.transpose(0, 1)
-	])
-print(train_x.shape)
-
-train_y = torch.stack([
-		nou_tr_y,
-		bits_tr_y,
-		pktnum_tr_y,
-		sig_tr_y
-	])
-print(train_y.shape)
-
-"""
-train_x = bits_tr_x.transpose(0, 1)
-train_y = bits_tr_y
-"""
-
-likelihood = gpytorch.likelihoods.GaussianLikelihood()
-gp_model = gptu.LinearGPModel(train_x, train_y, likelihood)
-optimizer = torch.optim.Adam([
-		{"params" : gp_model.parameters()},
-	],
-	lr = 0.1
-)
+	raw_titles = [
+		"Number of Users",
+		"Bits",
+		"Number of Packets",
+		"Signal Strength",
+		"802.11a data"
+	]
 	
-gptu.TorchTrain(train_x, train_y, gp_model, likelihood, optimizer, 10)
-#save_as_csv("raw_training_signalstrength_15weeks_nosample_singlecol.csv", x_list) 
-#save_as_csv("raw_testing_signalstrength_15weeks_nosample_col.csv", y_list)
+	#pu.plot_features(raw_data, raw_titles)
+	
+	nou = buffer_filter(raw_nou_data)
+	bits = buffer_filter(raw_bits_data)
+	pktnum = buffer_filter(raw_pkt_data)
+	sig = buffer_filter(raw_sig_data)
+	phya = buffer_filter(raw_phya_data)
+
+
+	nou = sp.std_normalization(nou)
+	bits = sp.std_normalization(bits)
+	pktnum = sp.std_normalization(pktnum)
+	sig = sp.std_normalization(sig)
+	phya = sp.std_normalization(phya)
+
+	#pu.general_plot(bits, "All bits")
+	#pu.general_plot(phya, "Phya bits")
+	
+	filtered_data = [nou, bits, pktnum, sig, phya]
+	filter_str = "Filtered "
+	filtered_titles = [
+		filter_str+raw_titles[0],
+		filter_str+raw_titles[1],
+		filter_str+raw_titles[2],
+		filter_str+raw_titles[3],
+		filter_str+raw_titles[4]
+	]
+		
+	#pu.plot_features(filtered_data, filtered_titles)
+	
+	
+	#pu.plot_autocorr(nou, "Auto correlation of number of users")
+	#pu.plot_autocorr(bits, "Auto correlation of bits")
+	#pu.plot_autocorr(pktnum, "Auto correlation of number of packets")
+	#pu.plot_autocorr(sig, "Auto correlation of signal strength")
+	#pu.plot_autocorr(phya, "Auto correlation of 802.11a bits")
+	
+	#pu.plot_crosscorr(bits, nou, "Cross correlation between number of users and bits")
+	#pu.plot_crosscorr(bits, pktnum, "Cross correlation between bits and number of packets")
+	#pu.plot_crosscorr(pktnum, sig, "Cross correlation between number of packets and signal strength")
+	
+	nou_tr_x, nou_tr_y = window_prep(nou)
+	bits_tr_x, bits_tr_y = window_prep(bits)
+	pktnum_tr_x, pktnum_tr_y = window_prep(pktnum)
+	sig_tr_x, sig_tr_y = window_prep(sig)
+	
+	phya_tst_x, phya_tst_y = window_prep(phya)
+	
+	train_x = torch.stack([
+			nou_tr_x.transpose(0, 1),
+			bits_tr_x.transpose(0, 1),
+			pktnum_tr_x.transpose(0, 1),
+			sig_tr_x.transpose(0, 1)
+		])
+	print(train_x.shape)
+	
+	train_y = torch.stack([
+			nou_tr_y,
+			bits_tr_y,
+			pktnum_tr_y,
+			sig_tr_y
+		])
+	print(train_y.shape)
+	
+	#train_x = bits_tr_x.transpose(0, 1)
+	#train_y = bits_tr_y
+	
+	likelihood = gpytorch.likelihoods.GaussianLikelihood()
+	gp_model = gptu.LinearGPModel(train_x, train_y, likelihood)
+	optimizer = torch.optim.Adam([
+			{"params" : gp_model.parameters()},
+		],
+		lr = 0.5
+	)
+		
+	gptu.TorchTrain(train_x, train_y, gp_model, likelihood, optimizer, 100)
+	
+	test_x = torch.stack([
+		phya_tst_x.transpose(0, 1)
+	])
+
+	test_y = torch.stack([
+		phya_tst_y
+	])
+
+	print(test_x.shape, test_y.shape)
+
+	pred = gptu.TorchTest(test_x, gp_model, likelihood)
+	print(pred.mean.numpy().shape)
+	print("Confidence region shape:")
+	print(pred.confidence_region()[0].detach().numpy().shape)
+	print(pred.mean.numpy().shape[0])
+	#if pred.mean[0, 0]:
+	#	print("hello")
+		
+
+	time_x = torch.Tensor([i for i in range(train_y.shape[1])])
+
+	pred_title = "Linear GP Prediction\nwith 1 standard deviation\nand 2 standard deviations"
+	x_title = "Time (hours)"
+	y_title = "Bits"
+	pu.PlotMTGPPred(time_x, torch.Tensor(train_y), time_x, pred, x_title, y_title, pred_title)
